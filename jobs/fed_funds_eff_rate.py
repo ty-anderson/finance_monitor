7e64b66316d9d4f4387220e14bd0611a5026893f
Tyler Anderson <tyleranderson827@gmail.com>
2024-11-10 08:51:05 -0700

    update to new results

--- jobs/fed_funds_eff_rate.py
+++ jobs/fed_funds_eff_rate.py
@@ -5,13 +5,10 @@ Get Federal Funds Effective Rate
 Download the API data for Federal Funds Rate and Federal Funds Target Rate.
 If there's any new data, load to database and then report it via Discord Webhook
 """
-import datetime
 import pandas as pd
-from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
-from db.model import engine, fed_funds_eff_rate_tbl, fed_funds_target_tbl
+from db.model import engine, fed_funds_eff_rate_tbl
 from alert.discord import webhook_finance_monkey
-from jobs.funcs import format_table_with_pipes, plot_df_data
 from jobs.fed_funds_target_rate import etl as target_etl
 import requests
 from bs4 import BeautifulSoup
@@ -19,26 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def get_reporting_data(years_back: int):
-    today = datetime.date.today()
-    min_date = datetime.date(today.year - years_back, today.month, 1)
-
-    with engine.connect() as conn:
-        stmt = select(fed_funds_eff_rate_tbl.c.date,
-                      fed_funds_eff_rate_tbl.c.value).where(fed_funds_eff_rate_tbl.c.date > min_date)
-        df = pd.read_sql(stmt, conn)
-
-        target_stmt = select(fed_funds_target_tbl.c.date,
-                             fed_funds_target_tbl.c.value_upper,
-                             fed_funds_target_tbl.c.value_lower).where(fed_funds_target_tbl.c.date > (today - datetime.timedelta(days=10)))
-        df_target = pd.read_sql(target_stmt, conn)
-
-
-    return df, df_target
-
-
 def etl():
-    start_time = datetime.datetime.now()
     url = 'https://www.federalreserve.gov/feeds/Data/H15_H15_RIFSPFF_N.B.XML'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "xml")
@@ -54,23 +32,34 @@ def etl():
         insert_stmt = insert_stmt.on_conflict_do_nothing()
         conn.execute(insert_stmt)
         conn.commit()
+        rates_df = pd.read_sql(f"""
+        SELECT A.date,
+               A.value,
+               B.value_upper,
+               B.value_lower,
+               (B.value_upper + B.value_lower)/2 AS target_avg
+        FROM federal_funds_effective_rate A
+        LEFT JOIN public.federal_funds_target_rate B on A.id = B.id
+        WHERE A.date IN (
+            current_date,
+            current_date - 1,
+            current_date - 2,
+            current_date - 3,
+            current_date - 4,
+            current_date - 5,
+            date_trunc('month', current_date) - interval '1 month',
+            date_trunc('month', current_date) - interval '2 month',
+            date_trunc('month', current_date) - interval '3 month',
+            date_trunc('month', current_date) - interval '4 month',
+            date_trunc('month', current_date) - interval '5 month'
+        );
+        """, conn)
 
-    # send data info
-    rates, target_rates = get_reporting_data(years_back=2)
-    rates = rates.sort_values(by=['date'])
-    target_rates = target_rates.sort_values(by=['date'])
-    df_str = format_table_with_pipes(rates.astype(str))
-    target_str = format_table_with_pipes(target_rates.astype(str))
-
-    img_buffer = plot_df_data(rates)
     webhook_finance_monkey(f'New Federal Funds Effective Rate is out.\n'
-                           f'Here is the T24 month rates:\n\n\n'
-                           + df_str + '\n\n' + target_str,
-                           file=('plot.png', img_buffer, 'image/png'))
-    img_buffer.close()
+                           f'Federal funds rate:\n\n\n'
+                           + rates_df.to_string(index=False, justify='match-parent'))
 
 
 if __name__ == '__main__':
     target_etl()
     etl()
-    webhook_finance_monkey('Federal Funds Rate Done')
